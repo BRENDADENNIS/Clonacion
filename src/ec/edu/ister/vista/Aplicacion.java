e173de0edcb6cbeb4819a48a25d7a36011dce903
steban <josueayo.ejat@gmail.com>
2018-11-12 08:54:22 -0500

    DEBER_12112018 #QUEMADOS #PARES #MULTIPLOS

--- src/ec/edu/ister/vista/Aplicacion.java
+++ src/ec/edu/ister/vista/Aplicacion.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.ister.vista;
 
 import javax.swing.JOptionPane;
-/**
- *
- * @author Usuariopc
- */
+
 public class Aplicacion {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,factorial(5));
@@ -30,7 +23,7 @@ public static int pares(){
         x++;
         if (x%2==0){
             result=x;
-            JOptionPane.showMessageDialog(null,"numeros pares: "+(result) );
+            JOptionPane.showMessageDialog(null,"#PARES: "+(result) );
        }
     }
         return result;
